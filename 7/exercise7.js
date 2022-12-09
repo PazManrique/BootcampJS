c34a51b65ba820c6563a54ddaa80c73eaae47a9c
Manrique <maria-paz.manrique@capgemini.com>
2022-12-09 11:28:11 +0100

    adding exercise 7

---
+++ 7/exercise7.js
@@ -0,0 +1,21 @@
+/*
+EJERCITACION 7
+EJERCITACION 7
+1. Escribir el código de una función a la que se pasa como parámetro un número entero y devuelva como resultado una cadena de texto que indica si el número es par o impar.
+Mostrar por pantalla el resultado devuelto por la función.
+2. Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa
+Mostrar por pantalla el resultado devuelto por la función.
+2. Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa*/
+
+let number = parseInt(prompt("Introduce un número"));
+
+    function pair(number) {
+        if(number % 2 == 0) {
+          alert ("El número "+ number + " es par");
+        }
+        else {
+            alert ("El número "+ number + " es impar");
+        }
+}
+
+pair(number);
---
+++ README.md
@@ -0,0 +1,5 @@
+# GitHub
+
+git remote add origin https://github.com/PazManrique/BootcampJS.git
+git branch -M main
+git push -u origin main
